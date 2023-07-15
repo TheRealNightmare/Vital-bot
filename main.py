68275557016337da0b24d90f3457f6b39fbeb5a2
Mirazul Islam Nahid <84902415+TheRealNightmare@users.noreply.github.com>
2023-07-15 16:28:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import yt_dlp
 import asyncio
 import os
 
-TOKEN = 'Token here'
+TOKEN = 'Bot Token'
 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -12,34 +12,12 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 # Load the opus library
 discord.opus.load_opus('libopus.so.0')
 
-@bot.command()
-async def play(ctx, url: str):
-    voice_channel = discord.utils.get(ctx.guild.voice_channels, name=ctx.author.voice.channel.name)
-    if not voice_channel:
-        await ctx.send("You are not connected to a voice channel")
-        return
-
-    if not ctx.voice_client:
-        voice_client = await voice_channel.connect()
-    else:
-        voice_client = ctx.voice_client
-        if voice_client.channel != voice_channel:
-            await voice_client.move_to(voice_channel)
-
-    with yt_dlp.YoutubeDL({'format': 'bestaudio/best', 'noplaylist': True}) as ydl:
-        try:
-            info = ydl.extract_info(url, download=True)
-            filename = ydl.prepare_filename(info)
-            source = discord.FFmpegOpusAudio(filename)
-        except Exception as e:
-            await ctx.send(f"Error: {e}")
-            return
-
-    def after_play(error):
-        os.remove(filename)
-
-    voice_client.play(source, after=after_play)
-    await ctx.send(f'Now playing: {info["title"]}')
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+    voice_channel = discord.utils.get(bot.get_all_channels(), type=discord.VoiceChannel)
+    if voice_channel:
+        await voice_channel.connect()
 
 @bot.command()
 async def leave(ctx):
@@ -67,5 +45,56 @@ async def resume(ctx):
         await ctx.send("Resumed")
     else:
         await ctx.send("Nothing is paused")
+
+@bot.command()
+async def play(ctx, song: str):
+    voice_channel = discord.utils.get(ctx.guild.voice_channels, name=ctx.author.voice.channel.name)
+    if not voice_channel:
+        await ctx.send("You are not connected to a voice channel")
+        return
+
+    if not ctx.voice_client:
+        voice_client = await voice_channel.connect()
+    else:
+        voice_client = ctx.voice_client
+        if voice_client.channel != voice_channel:
+            await voice_client.move_to(voice_channel)
+
+    query = song
+    with yt_dlp.YoutubeDL({'format': 'bestaudio/best', 'noplaylist': True}) as ydl:
+        try:
+            info = ydl.extract_info(f"ytsearch:{query}", download=True)['entries'][0]
+            filename = ydl.prepare_filename(info)
+            source = discord.FFmpegOpusAudio(filename)
+        except Exception as e:
+            await ctx.send(f"Error: {e}")
+            return
+
+    def after_play(error):
+        os.remove(filename)
+        if len(queue) > 0:
+            next_song = queue.pop(0)
+            bot.loop.create_task(play(ctx, next_song))
+
+    if ctx.voice_client.is_playing() or ctx.voice_client.is_paused():
+        queue.append(song)
+        await ctx.send(f'{song} added to the queue')
+    else:
+        voice_client.play(source, after=after_play)
+        await ctx.send(f'Now playing: {info["title"]}')
+
+queue = []  # List to store the queued songs
+
+@bot.command()
+async def skip(ctx):
+    voice_client = ctx.voice_client
+    if voice_client and voice_client.is_playing():
+        voice_client.stop()
+        await ctx.send("Skipped the current song")
+        if len(queue) > 0:
+            next_song = queue.pop(0)
+            bot.loop.create_task(play(ctx, next_song))
+    else:
+        await ctx.send("Nothing is currently playing")
 
 bot.run(TOKEN)
